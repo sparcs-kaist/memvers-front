483a11477d2008eaf174c8f8dda2f311ea79549a
Jinwoo Jeong (Leo) <thinkratomos@gmail.com>
2019-03-09 18:20:25 +0900

    Add delete feature.

--- src/components/MenuForWheel/DeleteUser.js
+++ src/components/MenuForWheel/DeleteUser.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { TextField } from '@material-ui/core';
+import { TextField, Button } from '@material-ui/core';
+import axios from 'axios';
 
 export default class DeleteUser extends Component {
   state = {
@@ -9,24 +10,46 @@ export default class DeleteUser extends Component {
   }
 
   handleChange = (e, value) => {
-    if (value == 'name') this.setState({ username: e.target.value })
-    else if (value == 'verity') this.setState({ userInput: e.target.value })
+    if (value == 'name') this.setState({ userName: e.target.value })
+    else if (value == 'verify') this.setState({ userInput: e.target.value })
+  }
+
+  delete = async () => {
+    const { verifyText, userInput, userName } = this.state
+    if (verifyText == userInput) {
+      try {
+        const payload = await axios.post('https://memvers-api.sparcs.org/api/wheel/delete', {un: userName}, {withCredentials: true})
+        if (payload.data.expired) window.location.href = '/login'
+        else if (payload.data.result) alert('Delete success.')
+        else alert('No such user')
+      } catch (error) {
+        alert(error)
+      }
+    } else {
+      alert('Text does not match.')
+    }
   }
 
   render() {
+    console.log(this.state)
     return (
       <div>
         <TextField 
-          value={this.state.userName}
+          label="ID"
           onChange={(e) => this.handleChange(e, 'name')}
         />
         <div>
           삭제를 원할 경우, 아래 문구를 입력하세요.
-          {this.state.verifyText}
         </div>
         <TextField
+          label="삭제하겠습니다."
           onChange={(e) => this.handleChange(e, 'verify')}
         />
+        <Button
+          onClick={this.delete}
+        >
+          삭제
+        </Button>
       </div>
     )
   }
