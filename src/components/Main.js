ad83d191e76e58ce6dda7c9506e8f82aff19ab37
Jinwoo Jeong (Leo) <thinkratomos@gmail.com>
2019-03-09 20:48:52 +0900

    Change container style.

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,17 +5,16 @@ import Login from './Login'
 import Menu from './Menu'
 
 import MainStyle from './Main.css'
-import defaultStyle from './default.css'
 
 const Main = () => {
   return (
     <div className={MainStyle.mainContainer}>
-      <div className={defaultStyle.header}>
+      <div className={MainStyle.header}>
         <div style={{marginLeft: 10}}>
           Memvers
         </div>
       </div>
-      <div style={{marginTop: 50}}>
+      <div className={MainStyle.contentContainer}>
         <Switch>
           <Route path="/menu" component={Menu} />
           <Route path="/login" component={Login} />
