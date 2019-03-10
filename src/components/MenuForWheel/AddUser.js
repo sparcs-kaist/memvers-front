d4bbd8825e9eae92663f2659c3501147cdabecd5
Jinwoo Jeong (Leo) <thinkratomos@gmail.com>
2019-03-10 14:39:30 +0900

    [#22] Refine add user style.

--- src/components/MenuForWheel/AddUser.js
+++ src/components/MenuForWheel/AddUser.js
@@ -4,6 +4,8 @@ import { TextField, Button } from '@material-ui/core';
 import AddUserStyle from './AddUser.css'
 import axios from 'axios';
 
+import defaultStyle from '../MenuButtons/default.css'
+
 export default class AddUser extends Component {
   state = {
     id: '',
@@ -57,25 +59,35 @@ export default class AddUser extends Component {
   render() {
     return (
       <div className={AddUserStyle.container}>
+        <span className={defaultStyle.description}>
+          새로운 회원을 추가합니다. 환영해요!
+        </span>
         <TextField
+          style={{width: '100%', marginTop: 10}}
           label="ID"
           onChange={(e) => this.handleChange(e, 'id')}
         />
         <TextField
+          style={{width: '100%', marginTop: 10}}
           label="이름"
           onChange={(e) => this.handleChange(e, 'name')}
         />
         <TextField
+          style={{width: '100%', marginTop: 10}}
           label="비밀번호"
           onChange={(e) => this.handleChange(e, 'password')}
           type="password"
         />
         <TextField
+          style={{width: '100%', marginTop: 10}}
           label="비밀번호 확인"
           onChange={(e) => this.handleChange(e, 'cpassword')}
           type="password"
         />
         <Button
+          variant="contained"
+          color="primary"
+          style={{width: '100%', boxShadow: 'none', marginTop: 10}}
           onClick={this.add}
         >
           등록
