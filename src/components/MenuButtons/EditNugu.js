3c79cf847d04268b97b855570642caf5edefaa7d
Jinwoo Jeong (Leo) <thinkratomos@gmail.com>
2019-03-10 14:26:37 +0900

    [#22] Refine editing nugu info style.

--- src/components/MenuButtons/EditNugu.js
+++ src/components/MenuButtons/EditNugu.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 import EditNuguStyle from './EditNugu.css'
 import { TextField, Button, Checkbox } from '@material-ui/core';
 
+import defaultStyle from './default.css'
+
 import { propertyName } from '../utils'
 export default class EditNugu extends Component {
   state = {
@@ -94,8 +96,15 @@ export default class EditNugu extends Component {
   render() {
     return (
       <div className={EditNuguStyle.container}>
+        <span className={defaultStyle.description}>
+          Nugu 정보를 편집합니다. 아래의 정보는 Nugu 에서 나의 이름 / 아이디를 검색할 경우 노출됩니다. 또한 SPARCS 공식 홈페이지의 멤버 란에도 노출됩니다. 만약 SPARCS 공식 홈페이지에 노출되는 것을 원하지 않는다면, 비공개 여부를 설정할 수 있습니다.
+        </span>
+        <div style={{height: 20}}></div>
         {this.renderContent()}
         <Button
+          variant="contained"
+          color="primary"
+          style={{width: '100%', boxShadow: 'none', marginTop: 10}}
           onClick={this.save}
         >
           저장
