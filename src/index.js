d22578ecfe734fadba380aa7fbba47ea1ed3051d
Khinenw <deu07115@gmail.com>
2020-09-16 18:32:51 +0900

    fix: Add initial CSRF cookie retrieval

--- src/index.js
+++ src/index.js
@@ -3,21 +3,36 @@ import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { BrowserRouter } from 'react-router-dom'
 
+import { loginApi } from './api'
 import 'babel-polyfill'
 
 import Main from './components/Main'
 
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <BrowserRouter>
-        <Component />
-      </BrowserRouter>
-    </AppContainer>,
-    document.getElementById('root')
-  )
-}
+// Initialize CSRF cookie
+api.get('/', { validateStatus: false })
+  .then(({ data, status }) => {
+    if (status !== 418) {
+      return Promise.reject(
+        new Error("Server did not respond with correct status code!")
+      )
+    }
 
-render(Main);
+    Promise.resolve()
+  })
+  .then(() => {
+    const render = Component => {
+      ReactDOM.render(
+        <AppContainer>
+          <BrowserRouter>
+            <Component />
+          </BrowserRouter>
+        </AppContainer>,
+        document.getElementById('root')
+      )
+    }
 
-if (module.hot) module.hot.accept('./components/Main', () => render(Main));
+    render(Main);
+
+    if (module.hot) module.hot.accept('./components/Main', () => render(Main));
+  })
+  .catch(err => alert(err))
